3cc1d7b7d0e8780b1880809fa84109c21ebb6c11
Febryan Grady <febryangrady@gmail.com>
2021-06-13 18:24:23 +0700

    reservasi selesai.

--- JS/reservation.js
+++ JS/reservation.js
@@ -29,7 +29,39 @@ function validate() {
         $(".people-error").css("display", "none")
     }
     const d = new Date()
-    var dateVal = $("#date").val()
-    alert(dateVal.substr(0,2) + d.getDate )
+    var checkDate = new Date($("#date").val())
+    if((checkDate.getDate() <= d.getDate() && checkDate.getMonth() <= d.getMonth() && checkDate.getFullYear() <= d.getFullYear()) || $("#date").val() == ""){
+        $(".date-error").css("display", "initial")
+        valid = false
+    } else {
+        $(".date-error").css("display", "none")
+    }
+    if(parseInt($("#time").val().substr(0,2) )< 9 || parseInt($("#time").val().substr(0,2)) > 20 || $("#time").val() == ""){
+        $(".time-error").css("display", "initial")
+        valid = false
+    } else {
+        $(".time-error").css("display", "none")
+    }
+    const coupon = ["ABPSFEB", "ABMGJUL", "ABBDCOU"]
+    var usedCode = ""
+    coupon.forEach(function(code){
+        if(code == $("#coupon").val()){
+            usedCode += code
+        }
+    })
+    if($("#coupon").val() != "" && usedCode == ""){
+        valid = false
+        $(".coupon-error").css("display", "initial")
+    } else {
+        $(".coupon-error").css("display", "none")
+    }
+    const month = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+    if(valid){
+        if(usedCode != ""){
+            alert("Selamat "+ $("#name").val() +" ! Reservasi berhasil dilakukan!!! \nAnda telah menggunakan kode kupon : " + usedCode + "\nmohon datang pada jam " + $("#time").val() + " di tanggal " + checkDate.getDate() + " " + month[checkDate.getDate()] + " " + checkDate.getFullYear() + "\nkami akan menghubungi nomor " + $("#telp").val() + " untuk mengkonfirmasi reservasi." )
+        } else {
+            alert("Selamat "+ $("#name").val() +" ! Reservasi berhasil dilakukan!!! \nmohon datang pada jam " + $("#time").val() + " di tanggal " + checkDate.getDate() + " " + month[checkDate.getDate()] + " " + checkDate.getFullYear() + "\nkami akan menghubungi nomor " + $("#telp").val() + " untuk mengkonfirmasi reservasi.")  
+        }
+    }
     return false
 }
